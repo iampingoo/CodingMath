67be60ec29d97acef05635ef7fd051b9dc2dbb04
iampingoo <iampingoo@yahoo.co.jp>
2020-12-30 14:57:23 +0900

    Update main.js

--- episode1/main.js
+++ episode1/main.js
@@ -1,10 +1,10 @@
-window.onload = function() {
-	var canvas = document.getElementById("canvas"),
+window.onload => () {
+	const canvas = document.getElementById("canvas"),
 		context = canvas.getContext("2d"),
 		width = canvas.width = window.innerWidth,
 		height = canvas.height = window.innerHeight;
 
-	for(var i = 0; i < 100; i += 1) {
+	for(let i = 0; i < 100; i += 1) {
 		context.beginPath();
 		context.moveTo(Math.random() * width, Math.random() * height);
 		context.lineTo(Math.random() * width, Math.random() * height);
